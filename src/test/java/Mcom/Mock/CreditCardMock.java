0dd6c77f85b93501197d5ce449080696f53c3e82
aaratiInamdar <arti.sanas3555@gmail.com>
2025-09-12 14:57:33 +0530

    commit1

---
+++ src/test/java/Mcom/Mock/CreditCardMock.java
@@ -0,0 +1,36 @@
+package Mcom.Mock;
+
+import org.apache.commons.lang3.StringUtils;
+import com.jayway.jsonpath.JsonPath;
+import spark.Spark;
+
+public class CreditCardMock {
+
+	public static void main(String[] args) {
+	
+		
+		Spark.port(8889);
+		Spark.post("/credit-card", (req,res)->
+		{
+			String response= "";
+//			in json we have to use $ before using the json path 
+			String card=JsonPath.read(req.body().toString(), "$.creditcard" );
+			
+			if (StringUtils.equalsAny(card, "1234567891234" , "1234567899876"))
+			{
+				response = "{\"status\":\"Payment success\"}" ;
+				res.status(200);
+			}
+			else 
+			{
+				response ="{\"status\":\"Payment Failed\"}" ;
+				res.status(404);
+			}
+			res.type("application/json");
+			return response ;
+		});
+		
+		System.out.println("==============Running===========");
+	}
+
+}
